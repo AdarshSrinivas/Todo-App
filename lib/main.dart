66f240e0dc9c8c4305ce3b5be685586e8ad95d1a
Ashwin <ashwinsrini@gmail.com>
2021-10-25 21:28:54 +0530

    Changed Sizedbox height from 40 to 20

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -81,7 +79,7 @@ class _SplashScreenState extends State<SplashScreen> {
                   ),
                 ),
                 SizedBox(
-                  height: 40,
+                  height: 20,
                 ),
                 CircularProgressIndicator(
                   valueColor: AlwaysStoppedAnimation<Color>(
